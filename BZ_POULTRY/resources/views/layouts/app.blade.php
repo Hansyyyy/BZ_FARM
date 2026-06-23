6289b67ad5c927ca703bed4b90aed8d2f31d896a
ryan <co230470@adzu.edu.ph>
2026-06-23 13:28:47 +0800

    files organized for the dashbord files both admin and manager. css files to be inserted

--- BZ_POULTRY/resources/views/layouts/app.blade.php
+++ BZ_POULTRY/resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <title>@yield('title', 'Dashboard') - BZ Farm</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/farm.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin-dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/manager-dashboard.css') }}">
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.jsx'])
