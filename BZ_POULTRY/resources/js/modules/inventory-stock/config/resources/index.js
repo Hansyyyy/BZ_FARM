33ff612cdfeeba4f70256fa3e237032ba01ef824
dave <co230521@adzu.edu.ph>
2026-07-15 00:52:03 +0800

    Trying to fix

--- BZ_POULTRY/resources/js/modules/inventory-stock/config/resources/index.js
+++ BZ_POULTRY/resources/js/modules/inventory-stock/config/resources/index.js
@@ -1,18 +1,16 @@
-// import flocks from './flocks.js';
+import flocks from './flocks.js';
 import feed from './feed.js';
 import medicine from './medicine.js';
 import inventory from './inventory.js';
 import eggs from './eggs.js';
 
 export const resources = {
-    // flocks is not present in this folder (missing ./flocks.js)
-    // flocks,
-
+    flocks,
     feed,
     medicine,
     inventory,
     eggs,
 };
 
-export { feed, medicine, inventory, eggs };
+export { flocks, feed, medicine, inventory, eggs };
 
